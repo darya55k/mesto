1f6818af9ae54d8cbf37776d8740daeab85740e8
darya55k <darya55k2001@yandex.ru>
2021-03-17 22:24:53 +0300

    sec-rev

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@
 let popup = document.querySelector('.popup');
 let closePopupBtn = document.querySelector('.popup__close-icon')
 let openPopupBtn =document.getElementById('open_popup_btn');
-let popupTitle = document.querySelector('.form__item-title');
-let popupSubtitle = document.querySelector('.form__item-subtitle');
+let popupTitle = document.querySelector('.form__item_input_title');
+let popupSubtitle = document.querySelector('.form__item_input_subtitle');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let profileTitle = document.querySelector('.profile__title');
 let formElement = document.querySelector('.form__content');
@@ -23,15 +23,11 @@ function openPopup(){
     popupSubtitle.value = profileSubtitle.textContent;
 }
 
-openPopupBtn.addEventListener('click', function() {
-    openPopup();
-});
+openPopupBtn.addEventListener('click', openPopup);
 
 function closePopup(){
     popup.classList.remove('popup_open');
 }
 
-closePopupBtn.addEventListener('click', function() {
-    closePopup();
-});
+closePopupBtn.addEventListener('click', closePopup);
 
